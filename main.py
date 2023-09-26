7697032db9d0c071c1ca75b44c733df1c2a4044f
Francis Tweedy <francis.tweedy@braincorp.com>
2023-09-26 08:18:14 -0700

    Add api key to git ignore

--- main.py
+++ main.py
@@ -17,27 +17,60 @@ def main():
 
     llm = OpenAI(temperature=0.9)
 
-    # prompt = f"""You are a tech support expert that knows everything about the machine mentioned in the documents: {index}.\
-    #             Advise on the best way to solve the problems mentioned in the question delimited by the triple backticks.\
-    #             question: ```{question}```
-    #     into a style that is {style}.
-    #     text: ```{customer_email}```
-    #     """
-
     loader = PyPDFDirectoryLoader("./KnowledgeFiles/")
     index = VectorstoreIndexCreator(
         vectorstore_cls=DocArrayInMemorySearch
     ).from_loaders([loader])
+
+    from langchain import FewShotPromptTemplate
+
+    # create our examples
+    examples = [
+        {
+            "query": "How are you?",
+            "answer": "I can't complain but sometimes I still do."
+        }, {
+            "query": "What time is it?",
+            "answer": "It's time to get a watch."
+        }
+    ]
+
+    # create a example template
+    example_template = """
+    User: {query}
+    AI: {answer}
+    """
+
+    # create a prompt example from above template
+    example_prompt = PromptTemplate(
+        input_variables=["query", "answer"],
+        template=example_template
+    )
+
+    # now break our previous prompt into a prefix and suffix
+    # the prefix is our instructions
+    prefix = """The following are exerpts from conversations with an AI
+    assistant. The assistant is typically sarcastic and witty, producing
+    creative  and funny responses to the users questions. Here are some
+    examples: 
+    """
+    # and the suffix our user input and output indicator
+    suffix = """
+    User: {query}
+    AI: """
+
+    # now create the few shot prompt template
+    few_shot_prompt_template = FewShotPromptTemplate(
+        examples=examples,
+        example_prompt=example_prompt,
+        prefix=prefix,
+        suffix=suffix,
+        input_variables=["query"],
+        example_separator="\n\n"
+    )
     
 
-    # query = "How do I do a soft reboot?"
-    # query ="How do I turn the machine off?"
     query ="List all the steps to hard reboot the machine."
-    # query ="How do I restart the machine?"
-    # query ="My machine is not working."
-    # query ="Hello, how are you today?"
-    # query ="What is 2+2?"
-
     # Bad queries
     # query ="When do I hard reboot?" | Reason: AI ignore opinion question and says "You should hard reboot".
     # Doc does not contain information to support any opinion, answer should be "I don't know".
